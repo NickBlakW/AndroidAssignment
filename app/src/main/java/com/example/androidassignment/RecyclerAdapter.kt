5a28bfc1c1ef6704ecf87895b2743f7bb11da9bb
Nicolai Blak Walther <niwal19@student.sdu.dk>
2022-10-24 20:31:39 +0200

    Deleted unnecessary vars

--- app/src/main/java/com/example/androidassignment/RecyclerAdapter.kt
+++ app/src/main/java/com/example/androidassignment/RecyclerAdapter.kt
@@ -13,33 +13,6 @@ import com.google.android.material.snackbar.Snackbar
 
 class RecyclerAdapter(var m_list : List<Movie>, var context : Context) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    val titles = arrayOf(
-        "Django Unchained",
-        "Get Out",
-        "Gladiator",
-        "Pulp Fiction",
-        "Saving Private Ryan",
-        "Shrek",
-    )
-
-    val details = arrayOf(
-        "With the help of a German bounty-hunter, a freed slave sets out to rescue his wife from a brutal plantation-owner in Mississippi.",
-        "A young African-American visits his white girlfriend's parents for the weekend, where his simmering uneasiness about their reception of him eventually reaches a boiling point.",
-        "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
-        "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-        "Following the Normandy Landings, a group of U.S. soldiers go behind enemy lines to retrieve a paratrooper whose brothers have been killed in action.",
-        "A mean lord exiles fairytale creatures to the swamp of a grumpy ogre, who must go on a quest and rescue a princess for the lord in order to get his land back.",
-    )
-
-    val imgIds = arrayOf(
-        R.drawable.djangounchained,
-        R.drawable.getout,
-        R.drawable.gladiator,
-        R.drawable.pulpfiction,
-        R.drawable.ryan,
-        R.drawable.shrek,
-    )
-
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         var iImg : ImageView
         var iTitle : TextView
